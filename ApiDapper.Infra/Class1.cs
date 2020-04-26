f311d0670d2c848018fdc8cde0280412eb8f7f22
Marcio.Costa <marciodeath@hotmail.com>
2020-04-26 13:10:40 -0300

    Criação dos projetos

---
+++ ApiDapper.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiDapper.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ ApiDapper.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiDapper.Shared
+{
+    public class Class1
+    {
+    }
+}
