d69faceba8959ae6b48b48622f040c3ae34ab77c
Marcio.Costa <marciodeath@hotmail.com>
2020-04-27 11:52:31 -0300

    Metodos Cancel - Pay - Ship

--- ApiDapper.Domain/StoreContext/Entities/Customer.cs
+++ ApiDapper.Domain/StoreContext/Entities/Customer.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using ApiDapper.Domain.StoreContext.ValueObjects;
 
 namespace ApiDapper.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _address;
+
         public Customer(Name name,
                         Document document,
                         Email email,
@@ -14,14 +17,21 @@ namespace ApiDapper.Domain.StoreContext.Entities
             Document  = document;
             Email     = email;
             Phone     = phone;
-            Address   = new List<Address>();
+            _address   = new List<Address>();
         }
         
         public Name Name {get; private set;}
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Address { get; set; }
+        public IReadOnlyCollection<Address> Address => _address.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            // Validar o endereço
+            // Adicionar o endereço
+            _address.Add(address);
+        }
 
         public override string ToString()
         {
